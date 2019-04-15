01bdbb14971c4a7ff57e178d1e04186358ecc516
Matthew Blunden <c3303030@uon.edu.au>
2019-04-15 09:33:58 +0000

    Converting to uppercase

--- project.c
+++ project.c
@@ -3,17 +3,32 @@
 int main() {
     char* message = "Encrypt This! ZZZ";
     int key = 5;
+    
+    
+    printf("message: %c\n", input[i]);          // this print the input to line 5 in ASICII
+int i=0;
 
-    lower(n)
-    int n;
-    {
-        if ((n >=              ))
-        
-        
-        
-    }
+for (int i=0; input[i]!='\0'; i++)
+
+   if ((65<input[i]<90) ||  (97<input[i]<122))
+   {
+       temp = input[i];                     // temp stores values.
+       temp = temp - 65;
+       temp = temp + key;
+       temp = temp%26;
+       temp = temp + 65l
+       enc[i] = temp;
+       
+       
+   }
     
 
+else
+{
+    enc[1]  = input[1];
+}
 
+
+}
    
 
